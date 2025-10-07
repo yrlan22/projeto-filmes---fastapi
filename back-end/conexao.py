75b2592b88dbaff66c7cc9928f524df381e7d2dc
yrlan <nubdolinho@gmail.com>
2025-10-07 10:39:18 -0300

    conexao

---
+++ back-end/conexao.py
@@ -0,0 +1,25 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+#Carregar variaveis do .env
+load_dotenv()
+
+params = {
+    "dbname":os.getenv("DB_NAME"),
+    "user":os.getenv("DB_USER"),
+    "password":os.getenv("DB_PASSWORD"),
+    "host":os.getenv("DB_HOST"),
+    "port":os.getenv("DB_PORT")
+}
+
+def conectar():
+    try:
+        conexao = psycopg2.connect(**params)
+        cursor = conexao.cursor()
+        print("deu certo")
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexao{erro}")
+        return None, None
+conectar()
